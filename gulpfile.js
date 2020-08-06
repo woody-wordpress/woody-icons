fa41ad2402917685793d8d908fb164b90caf1105
Léo POIROUX <leo.poiroux@gmail.com>
2020-08-06 16:19:04 +0200

    Version 1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,153 @@
+'use strict';
+
+const gulp = require('gulp');
+const path = require('path');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const rev = require('gulp-rev');
+const sizereport = require('gulp-sizereport');
+const del = require('del');
+const iconfont = require('gulp-iconfont');
+const iconfontCss = require('gulp-iconfont-css');
+const runTimestamp = Math.round(Date.now() / 1000);
+const each = require('async-each-series');
+const fs = require('fs');
+var glob = require('glob');
+
+// ------------------
+// Gulp Tasks
+// ------------------
+gulp.task('icons', done => {
+    const folders = [
+        'icons_set_01',
+        'icons_set_02',
+        'icons_set_03',
+        'icons_set_04',
+        'icons_set_stations',
+        'shares'
+    ];
+
+    if (!fs.existsSync('./dist')) {
+        fs.mkdirSync('./dist');
+    }
+
+    each(folders, function (icon_set, next) {
+        gulp
+            .src('./icons/' + icon_set + '/**/*.svg')
+            .pipe(
+                iconfontCss({
+                    fontName: 'woody-icons',
+                    path: './icons/template/_icons.scss.tpl',
+                    targetPath: 'woody-icons.scss',
+                    fontPath: './',
+                    cacheBuster: runTimestamp,
+                    cssClass: 'wicon'
+                })
+            )
+            .pipe(
+                iconfont({
+                    fontName: 'woody-icons',
+                    prependUnicode: true, // recommended option
+                    formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'], // default, 'woff2' and 'svg' are available
+                    timestamp: runTimestamp, // recommended to get consistent builds when watching files
+                    fontHeight: 1001,
+                    normalize: true
+                })
+            )
+            .pipe(gulp.dest('./dist/' + icon_set))
+            .on('finish', () => {
+                glob('./icons/' + icon_set + '/**/*.svg', function (err, files) {
+                    var config_yml = './dist/' + icon_set + '/woody-icons.yml';
+                    fs.appendFile(config_yml, 'icons:\n', (err) => {
+                        if (err) throw err;
+                    });
+
+                    files.forEach((file) => {
+                        var filename = path.basename(file).replace('.svg', '');
+                        fs.appendFile(config_yml, '  - wicon-' + filename + '\n', (err) => {
+                            if (err) throw err;
+                        });
+                    })
+                });
+
+                // fs.appendFile('./dist/woody-icons.scss', '@import \'./' + icon_set + '/' + icon_set + '\';\n', (err) => {
+                //     if (err) throw err;
+                // });
+
+                next();
+            });
+
+    }, done);
+
+});
+
+
+gulp.task('css', () => {
+    return gulp
+        .src('./dist/**/*.scss')
+        .pipe(
+            sass({
+                outputStyle: 'expanded',
+                sourceMap: false,
+                errLogToConsole: true
+            })
+        )
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ['last 2 versions and > 0.5%'],
+                cascade: false
+            })
+        )
+        .pipe(
+            cleanCSS({
+                level: 2
+            })
+        )
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean', done => {
+    del.sync('./dist', {
+        force: true
+    });
+    done();
+});
+
+gulp.task('clean_watch', done => {
+    del.sync(['./dist/**/js', './dist/**/css'], {
+        force: true
+    });
+    done();
+});
+
+gulp.task('rev', () => {
+    return gulp
+        .src('./dist/**/*.+(css|js)')
+        .pipe(rev())
+        .pipe(gulp.dest('./dist')) // write rev'd assets to build dir
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('./dist')); // write manifest to build dir
+});
+
+gulp.task('size', () => {
+    return gulp.src('./dist/**/*.+(css|js)').pipe(
+        sizereport({
+            gzip: true
+        })
+    );
+});
+
+// Main tasks
+gulp.task(
+    'build',
+    gulp.series('clean', 'icons', 'css', 'rev', 'size')
+);
+gulp.task(
+    'build_watch',
+    gulp.series('clean_watch', 'icons', 'css', 'rev', 'size')
+);
+
+gulp.task('watch', () => {
+    gulp.watch('./icons/**/*.*', gulp.series('build_watch'));
+});
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+$icon_sets = [];
+foreach (glob('dist/*', GLOB_ONLYDIR) as $filename) {
+    $icon_sets[] = basename($filename);
+}
+
+$current_icon_set = [];
+if (!empty($_GET['s']) && in_array($_GET['s'], $icon_sets)) {
+    $current_icon_set['key'] = $_GET['s'];
+} else {
+    $current_icon_set['key'] = current($icon_sets);
+}
+
+$current_icon_set['name'] = ucfirst(str_replace('icons_set_', '', $current_icon_set['key']));
+
+$icons = yaml_parse_file(__DIR__ . '/dist/' . $current_icon_set['key'] . '/woody-icons.yml');
+$icons = $icons['icons'];
+sort($icons);
+$current_icon_set['icons'] = $icons;
+?>
+<!doctype html>
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <link rel='shortcut icon' type='image/x-icon' href="favicon.ico">
+    <title><?php echo $current_icon_set['name']; ?> | Woody Icons</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="/dist/<?php echo $current_icon_set['key']; ?>/woody-icons.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+
+    <style>
+        .wicon {
+            font-size: 60px;
+        }
+
+        h2 {
+            font-size: 11px;
+        }
+
+        .header {
+            max-width: 700px;
+        }
+
+        .card-deck .card {
+            min-width: 220px;
+        }
+    </style>
+    </head>
+
+    <body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <span class="my-0 mr-md-auto font-weight-normal"></span>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <?php foreach ($icon_sets as $icon_set): ?>
+                <a class="p-2 text-dark" href="/?s=<?php echo $icon_set; ?>"><?php echo ucfirst(str_replace('icons_set_', '', $icon_set)); ?></a>
+            <?php endforeach; ?>
+        </nav>
+    </div>
+
+    <div class="header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4">Icônes <?php echo $current_icon_set['name']; ?></h1>
+        <!--<p class="lead"></p>-->
+    </div>
+
+    <div class="container-fluid">
+        <div class="card-deck mb-3 text-center">
+            <?php foreach ($current_icon_set['icons'] as $icon): ?>
+            <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                    <h2 class="my-0 font-weight-normal"><?php echo $icon; ?></h2>
+                </div>
+                <div class="card-body">
+                    <span class="wicon wicon-woody-icons <?php echo $icon; ?>"></span>
+                    <a href="/icons/icons_set_01/<?php echo str_replace('wicon-', '', $icon); ?>.svg" class="btn btn-sm btn-block btn-outline-primary">Télécharger</a>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+
+        <footer class="pt-4 my-md-5 pt-md-5 border-top">
+            <div class="row">
+                <div class="col-12 col-md">
+                    <!-- <img class="mb-2" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">-->
+                    <small class="d-block mb-3 text-muted">© <?php echo date('Y'); ?></small>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    </body>
+</html>
